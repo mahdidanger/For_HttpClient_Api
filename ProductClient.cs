e9c655cf52bde457089cf177a28825d51566870f
mahdidanger <mahdikingok99@gmail.com>
2024-10-11 15:55:35 +0330

    Initial commit

---
+++ ProductClient.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ForAPI
+{
+    public class ProductClient
+    {
+        private readonly HttpClient _client;
+
+        public ProductClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task GetProductsAsync()
+        {
+            var response = await _client.GetAsync("https://localhost:7160/api/products");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonData);
+            }
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            
+            var json = JsonSerializer.Serialize(product);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("https://localhost:7160/api/products", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Product added successfully.");
+            }
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ForAPI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new ProductClient();
+
+        // دریافت محصولات
+        await client.GetProductsAsync();
+
+        // افزودن محصول جدید
+        var newProduct = new Product { Name = "Product1", Price = 99.99M };
+        await client.AddProductAsync(newProduct);
+    }
+}
+
